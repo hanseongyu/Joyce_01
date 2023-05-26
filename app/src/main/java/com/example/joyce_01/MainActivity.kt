1b2444aacdaefce484d9512836872532ca907c64
richer <persona1241@naver.com>
2023-05-26 19:01:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/joyce_01/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.joyce_01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        settingButton()
+
+
+        }
+    fun settingButton(){
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this,SubActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/joyce_01/TwoColorACtivity.kt
@@ -0,0 +1,13 @@
+package com.example.joyce_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TwoColorACtivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_two_color)
+    }
+
+
+}
